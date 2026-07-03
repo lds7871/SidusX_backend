53f36f461bffb5332191953bd0111e204629b286
LDS7871 <1964960588@qq.com>
2026-07-03 17:17:56 +0800

    Add initial configuration files and Swagger UI support
    - Added appsettings.example.json for application configuration with connection strings, security settings, mail settings, proxy configuration, WebSocket console settings, Swagger settings, logging configuration, and session management.
    - Introduced web.config for ASP.NET Core hosting configuration.
    - Included Swashbuckle.AspNetCore.SwaggerUI.dll for API documentation support.

--- obj/Debug/net10.0/SidusX_backend.AssemblyInfo.cs
+++ obj/Debug/net10.0/SidusX_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SidusX_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee811829068a0a33c38472cf8fb5250ff9b2c71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b110cad78937182e3af5da7b3d25f59742de5d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SidusX_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SidusX_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
